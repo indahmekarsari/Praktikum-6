6acb8197700ff379c8dfe60e1cb83d77e17cc21f
indahmekarsari <120694861+indahmekarsari@users.noreply.github.com>
2023-04-17 12:28:02 +0700

    Add files via upload

---
+++ 1_praktikum nomer 2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> Pemrograman PHP dengan Array </title>
+    </head>
+    <body>
+        <?php 
+        //Penulisan array dapat dibuat seperti berikut ini
+        $nama[] = "Rhohmah ";
+        $nama[] = "Indah ";
+        $nama[] = "Mekar Sari ";
+        echo $nama[0] . $nama[1] . $nama[2];
+        echo "<br>"
+        ?>
+    </body>
+</html>
